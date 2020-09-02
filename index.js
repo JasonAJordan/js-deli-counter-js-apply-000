44ef88ff61e439396375f16fa09c22c828ae0a76
Jason Jordan <jasonajordan0@gmail.com>
2020-09-02 19:51:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  
-  return "Welcome, " + name + ". You are number " + num + " in line."
+  let num = katzDeliLine.length;
+  return `Welcome, ${name}. You are number ${num} in line.`
 }
+
+function 
